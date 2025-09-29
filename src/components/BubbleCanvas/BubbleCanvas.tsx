202e2a622631713497c6b3e905074af472831734
SaeedAlsenani <Saeed111ahamed111@gmail.com>
2025-09-29 05:14:35 +0300

    Update BubbleCanvas.tsx

--- src/components/BubbleCanvas/BubbleCanvas.tsx
+++ src/components/BubbleCanvas/BubbleCanvas.tsx
@@ -152,7 +152,7 @@ const BubbleCanvas: React.FC<BubbleCanvasProps> = ({
     }
 
     return (
-        <div ref={containerRef} className="relative w-full h-full bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+        <div ref={containerRef} className="relative w-full h-full bg-gradient-to-br from-black via-gray-900 to-black
             <svg 
                 ref={svgRef} 
                 className="w-full h-full"
